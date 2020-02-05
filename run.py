8b52ada638e54ca7f80bba6b4f2833d9012bf756
Abishek V Ashok <abishekvashok@gmail.com>
2020-02-05 17:33:46 +0530

    Modifies count detection technique
    Instead of detecting each cycle of joint, try to detect the average
    movement across all joints thereby producing more accurate results.
    Previously the count increased rapidly per each cycle when they never
    superimposed. By taking average cycle across all joints we can solve
    this problem.
    
    Other minor changes:
     - Few variables have been renamed to better suite their purpose.
     - Default tolerance value has been tweaked to 30.
    
    Signed-off-by: Abishek V Ashok <abishekvashok@gmail.com>

--- run.py
+++ run.py
@@ -6,8 +6,7 @@ import argparse
 import posenet
 
 parser = argparse.ArgumentParser()
-# The best known value of variance is yet to be known, I assume it as 40.
-parser.add_argument('--variance', type=int, default=40, help='The tolerance for the model in integers')
+parser.add_argument('--tolerance', type=int, default=30, help='The tolerance for the model in integers')
 parser.add_argument('--model', type=int, default=101, help='The model to use, available versions are 101 (def.), 102, 103 etc')
 parser.add_argument('--cam_id', type=int, default=0, help='The respective cam id to use (default 0)')
 parser.add_argument('--cam_width', type=int, default=1280, help='The width of the webcam in pixels (def. 1280)')
@@ -19,39 +18,41 @@ args = parser.parse_args()
 keyValues = ['Nose', 'Left eye', 'Right eye', 'Left ear', 'Right ear', 'Left shoulder',
              'Right shoulder', 'Left elbow', 'Right elbow', 'Left wrist', 'Right wrist',
              'Left hip', 'Right hip', 'Left knee', 'Right knee', 'Left ankle', 'Right ankle']
-variance = args.variance
+tolerance = args.tolerance
 
-def countRepetition(previous_pose, current_pose, previous_novel, flag):
+def countRepetition(previous_pose, current_pose, previous_state, flag):
     if current_pose[0][10][0] == 0 and current_pose[0][10][1] == 0:
-        return 'Cannot detect any joint in the frame', previous_pose, previous_novel, flag
+        return 'Cannot detect any joint in the frame', previous_pose, previous_state, flag
     else:
-        string = ''
-        novel_string = ''
+        string, current_state = '', previous_state.copy()
+        sdy,sdx = 0,0
         # Discard first 5 (0-4 indices) values, we don't need the value of nose, eye etc
         for i in range(5, 17):
             # The fancy text overlay
             string = string + keyValues[i] + ': '
             string = string + str('%.2f' % (current_pose[0][i][0])) + ' ' + str('%.2f' % current_pose[0][i][1]) + '\n'
-            # If the difference is greater or lesser than variance or -variance indicate increase or decrease
-            # respectively. If no such situation occurs, copy the previous value
+            # If the difference is greater or lesser than tolerance or -tolerance sum it up or add 0 to sdx and sdy
             dx = (current_pose[0][i][0] - previous_pose[0][i][0])
             dy = (current_pose[0][i][1] - previous_pose[0][i][1])
-            if(dx > variance):
-                novel_string += '1'
-            elif(dx < (-1 * variance)):
-                novel_string += '0'
-            else:
-                novel_string += previous_novel[((i-5)*2)]
-            if(dy > variance):
-                novel_string += '1'
-            elif(dy < (-1 * variance)):
-                novel_string += '0'
-            else:
-                novel_string += previous_novel[((i-5)*2)+1]
-        # Increase flag if there is a change in the slope of the graph
-        if(novel_string != previous_novel):
-            flag += 1 # Not flag = 1 bcz flag is initialized to -1
-        return string, current_pose, novel_string, flag
+            if(dx < tolerance and dx > (-1 * tolerance)):
+                dx = 0
+            if(dy < tolerance and dy > (-1 * tolerance)):
+                dy = 0
+            sdx += dx
+            sdy += dy
+        # if an overall average increase in value is detected set the current_state's bit to 1, if it decrease set it to 0
+        # if it is between tolerance*3 and -tolerance*3, do nothing (then current_state will contain same value as previous)
+        if(sdx > (tolerance*3)):
+            current_state[0] = 1
+        elif(sdx < (tolerance*-3)):
+            current_state[0] = 0
+        if(sdy > (tolerance*3)):
+            current_state[1] = 1
+        elif(sdy < (tolerance*-3)):
+            current_state[1] = 0
+        if(current_state != previous_state):
+            flag = (flag + 1)%2
+        return string, current_pose, current_state.copy(), flag
 
 def main():
     with tf.Session() as sess:
@@ -70,9 +71,9 @@ def main():
         # A flag denoting change in state. 0 -> previous state is continuing, 1 -> state has changed
         flag = -1
         # Novel string stores a pair of bits for each of the 12 joints denoting whether the joint is moving up or down
-        # when plotted in a graph against time, 1 denotes upward and 0 denotes downward curving of the graph. Assume
-        # we move upward first, all motions ideally start from the ground
-        novel_string = '1' * 24
+        # when plotted in a graph against time, 1 denotes upward and 0 denotes downward curving of the graph. It is initialised
+        # as '22' so that current_state wont ever be equal to the string we generate unless there is no movement out of tolerance
+        current_state = [2,2]
         while True:
             # Get a frame, and get the model's prediction
             input_image, display_image, output_scale = posenet.read_cap(
@@ -94,11 +95,11 @@ def main():
             if(previous_pose == ''): # if previous_pose was not inialised, assign the current keypoints to it
                 previous_pose = keypoint_coords
             
-            text, previous_pose, novel_string, flag = countRepetition(previous_pose, keypoint_coords, novel_string, flag)
+            text, previous_pose, current_state, flag = countRepetition(previous_pose, keypoint_coords, current_state, flag)
 
             if(flag == 1):
                 count += 1
-                flag = 0
+                flag = -1
 
             image = posenet.draw_skel_and_kp(
                 display_image, pose_scores, keypoint_scores, keypoint_coords,
